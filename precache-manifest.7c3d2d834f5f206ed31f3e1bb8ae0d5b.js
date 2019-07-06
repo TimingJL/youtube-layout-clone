9a62f05fff8e8a50ca38ef744523ab4a29b791e1
Taiming Chen <taiming@bridgewell.com>
2019-07-06 12:11:54 +0800

    Updates

---
+++ precache-manifest.7c3d2d834f5f206ed31f3e1bb8ae0d5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cd34fd45a73b76d61d5a8326a89130",
+    "url": "/youtube-layout-clone/index.html"
+  },
+  {
+    "revision": "1cad2995f5bf3a713fd6",
+    "url": "/youtube-layout-clone/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/youtube-layout-clone/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1cad2995f5bf3a713fd6",
+    "url": "/youtube-layout-clone/static/js/main.0296621e.chunk.js"
+  },
+  {
+    "revision": "c14580471eae66f1c255",
+    "url": "/youtube-layout-clone/static/js/runtime~main.1e3e8848.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/youtube-layout-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
