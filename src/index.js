a09537f3932667996018a2a42f7d6069b5f01bfc
Taiming Chen <taiming@bridgewell.com>
2019-07-06 22:38:48 +0800

    add ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from 'Styled/Settings/colors';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={defaultTheme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
