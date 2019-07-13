aec858cc7d4c1666d0fdfd6f9b6c6efd7dcf3a53
Taiming Chen <taiming@bridgewell.com>
2019-07-14 00:25:43 +0800

    add ability to switch themeType

--- src/Styled/Settings/constants.js
+++ src/Styled/Settings/constants.js
@@ -6,6 +6,17 @@ export const BREAK_POINT_MD = 'md';
 export const BREAK_POINT_LG = 'lg';
 export const BREAK_POINT_XL = 'xl';
 
+export const themeOptions = [
+  {
+    id: 'lightTheme',
+    name: '預設主題',
+  },
+  {
+    id: 'darkTheme',
+    name: '深色主題',
+  },
+];
+
 export const breakpoints = {
   [BREAK_POINT_SM]: '657px',
   // [BREAK_POINT_SM]: '576px',
--- src/containers/YoutubeClone/NavigationBar/Avatar/MenuContent/index.js
+++ src/containers/YoutubeClone/NavigationBar/Avatar/MenuContent/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { findAttributeInEvent } from 'utils/event';
+import {
+  YoutubeCloneContext,
+} from 'containers/YoutubeClone/Context';
 import { AVATAR_URL } from 'containers/YoutubeClone/NavigationBar/constants';
+import {
+  themeOptions,
+} from 'Styled/Settings/constants';
+
+const themeSwitchIconAnimation = keyframes`
+  0% {
+    transform: scale(0.9);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+const setOptionStyle = (optionId, themeType) => (optionId === themeType ? 'active' : '');
 
 const MenuContentContainer = styled.div`
   .menu-content__profile-wrapper {
@@ -31,22 +52,94 @@ const MenuContentContainer = styled.div`
     font-weight: 500;
     cursor: pointer;
   }
+
+  .menu-content__theme-switch-wrapper {
+    font-size: 14px;
+    padding: 8px 0px;
+    border-bottom: 1px solid ${(props) => props.theme[props.themeType].dropdown.borderColor};
+  }
+  .menu-content__theme-switch-item {
+    display: flex;
+    align-items: center;
+    line-height: 40px;
+    font-size: 16px;
+    color: ${(props) => props.theme[props.themeType].dropdown.iconColor};
+    &.subject {
+      color: ${(props) => props.theme[props.themeType].dropdown.iconColor};
+    }
+    &.option {
+      cursor: pointer;
+      &:hover {
+        background: ${(props) => props.theme[props.themeType].dropdown.itemHoverBackground};
+      }
+    }
+    &.active {
+      color: ${(props) => props.theme[props.themeType].dropdown.color};
+    }
+  }
+  .menu-content__theme-switch-icon {
+    margin: 0px 16px;
+    font-size: 20px;
+    width: 24px;
+    text-align: center;
+    visibility: hidden;
+    &.subject {
+      color: ${(props) => props.theme[props.themeType].dropdown.iconColor};
+      visibility: visible;
+    }
+    &.option {
+      font-size: 16px;
+    }
+    &.active {
+      visibility: visible;
+      animation: ${themeSwitchIconAnimation} 0.2s ease-in-out;
+    }
+  }
 `;
 
 const MenuContent = ({
   themeType,
-}) => (
-  <MenuContentContainer themeType={themeType}>
-    <div className="menu-content__profile-wrapper">
-      <img className="menu-content__profile-avatar-image" src={AVATAR_URL} alt="" />
-      <div>
-        <div className="menu-content__profile-name">陳泰銘</div>
-        <div className="menu-content__profile-email">eefozeo@gmail.com</div>
-        <div className="menu-content__profile-management">管理你的 Google 帳戶</div>
+}) => {
+  const {
+    setThemeType,
+  } = useContext(YoutubeCloneContext);
+  const handleOnThemeClick = useCallback((event) => {
+    const selectedThemeOption = findAttributeInEvent(event, 'data-theme-option');
+    setThemeType(selectedThemeOption);
+  }, [setThemeType]);
+  return (
+    <MenuContentContainer themeType={themeType}>
+      <div className="menu-content__profile-wrapper">
+        <img className="menu-content__profile-avatar-image" src={AVATAR_URL} alt="" />
+        <div>
+          <div className="menu-content__profile-name">陳泰銘</div>
+          <div className="menu-content__profile-email">eefozeo@gmail.com</div>
+          <div className="menu-content__profile-management">管理你的 Google 帳戶</div>
+        </div>
       </div>
-    </div>
-  </MenuContentContainer>
-);
+      <div className="menu-content__theme-switch-wrapper">
+        <div className="menu-content__theme-switch-item subject">
+          <i className="fas fa-sun menu-content__theme-switch-icon subject" />
+          <div>自訂主題</div>
+        </div>
+        {
+          themeOptions.map((option) => (
+            <div
+              role="presentation"
+              key={option.id}
+              data-theme-option={option.id}
+              className={`menu-content__theme-switch-item option ${setOptionStyle(option.id, themeType)}`}
+              onClick={handleOnThemeClick}
+            >
+              <i className={`fas fa-check menu-content__theme-switch-icon option ${setOptionStyle(option.id, themeType)}`} />
+              <div className="menu-content__theme-switch-title">{option.name}</div>
+            </div>
+          ))
+        }
+      </div>
+    </MenuContentContainer>
+  );
+};
 
 MenuContent.propTypes = {
   themeType: PropTypes.string,
