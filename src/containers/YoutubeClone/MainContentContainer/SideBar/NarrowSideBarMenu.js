63fdab07aa9804e5efd39af55e62a05e797693da
Taiming Chen <taiming@bridgewell.com>
2019-08-02 12:19:28 +0800

    add translation to narrow sidebar

--- src/containers/YoutubeClone/MainContentContainer/SideBar/NarrowSideBarMenu.js
+++ src/containers/YoutubeClone/MainContentContainer/SideBar/NarrowSideBarMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
 import { isSelected } from './utils';
 
 const Icon = styled.div`
@@ -28,33 +29,41 @@ const Icon = styled.div`
   }
   .narrow-sidebar__text {
     font-weight: ${(props) => (props.isSelected ? 600 : 400)};
-    color: ${(props) => (props.isSelected ? props.theme.sidebar.iconActiveColor : 'currentColor')}
+    color: ${(props) => (props.isSelected ? props.theme.sidebar.iconActiveColor : 'currentColor')};
+    width: 70px;
+    overflow : hidden;
+    text-overflow : ellipsis;
+    white-space : nowrap;
+    text-align: center;
   }
 `;
 
 const NarrowSideBarMenu = ({
   selectedMenuItem,
   handleOnClick,
-}) => (
-  <>
-    <Icon data-menu-item="home" onClick={handleOnClick} isSelected={isSelected('home', selectedMenuItem)}>
-      <i className="fas fa-home narrow-sidebar__icon" />
-      <div className="narrow-sidebar__text">首頁</div>
-    </Icon>
-    <Icon data-menu-item="trending" onClick={handleOnClick} isSelected={isSelected('trending', selectedMenuItem)}>
-      <i className="fab fa-hotjar narrow-sidebar__icon" />
-      <div className="narrow-sidebar__text">發燒影片</div>
-    </Icon>
-    <Icon data-menu-item="subscriptions" onClick={handleOnClick} isSelected={isSelected('subscriptions', selectedMenuItem)}>
-      <i className="fab fa-youtube narrow-sidebar__icon" />
-      <div className="narrow-sidebar__text">訂閱內容</div>
-    </Icon>
-    <Icon data-menu-item="library" onClick={handleOnClick} isSelected={isSelected('library', selectedMenuItem)}>
-      <i className="fas fa-folder narrow-sidebar__icon" />
-      <div className="narrow-sidebar__text">媒體庫</div>
-    </Icon>
-  </>
-);
+}) => {
+  const { t } = useTranslation('sidebar');
+  return (
+    <>
+      <Icon data-menu-item="home" onClick={handleOnClick} isSelected={isSelected('home', selectedMenuItem)}>
+        <i className="fas fa-home narrow-sidebar__icon" />
+        <div className="narrow-sidebar__text">{t('home')}</div>
+      </Icon>
+      <Icon data-menu-item="trending" onClick={handleOnClick} isSelected={isSelected('trending', selectedMenuItem)}>
+        <i className="fab fa-hotjar narrow-sidebar__icon" />
+        <div className="narrow-sidebar__text">{t('trending')}</div>
+      </Icon>
+      <Icon data-menu-item="subscriptions" onClick={handleOnClick} isSelected={isSelected('subscriptions', selectedMenuItem)}>
+        <i className="fab fa-youtube narrow-sidebar__icon" />
+        <div className="narrow-sidebar__text">{t('subscriptions')}</div>
+      </Icon>
+      <Icon data-menu-item="library" onClick={handleOnClick} isSelected={isSelected('library', selectedMenuItem)}>
+        <i className="fas fa-folder narrow-sidebar__icon" />
+        <div className="narrow-sidebar__text">{t('library')}</div>
+      </Icon>
+    </>
+  );
+};
 
 NarrowSideBarMenu.propTypes = {
   selectedMenuItem: PropTypes.string,
